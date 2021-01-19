6df9be11df5e55e250da8739f8fd7912dcea8e71
pascalbenimana6 <53511483+pascalbenimana6@users.noreply.github.com>
2021-01-19 14:47:05 -0600

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,12 @@
+package edu.miu.cs.cs401.project.domain;
+
+import java.time.LocalDate;
+
+public class Passenger {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthDate;
+	private String emailAddress;
+	private Address residenceAddress;
+}
